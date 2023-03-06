ac3221ceab5907df6a6237080e82ab350a8c3ab2
Cris Moen <cris.moen@runtosolve.com>
2023-03-06 10:34:17 -0500

    draft HW4 solution, needs section props and loads

--- Spring_2023/HW4/HW4_Salginatoble_Tank.jl
+++ Spring_2023/HW4/HW4_Salginatoble_Tank.jl
@@ -1,41 +1,48 @@
 #EN.560.301 Structural Systems I 
-#HW 3 solution
-#February 2023
+#HW 4 solution
+#March 2023
 
 using LinearAlgebra
 
-#Calculate the deformation of a 2D joist truss cell.
+#Calculate displacements and internal forces in the Salginatoble bridge.
 
 #Define section properties.
-A = [0.32, 0.877]
+A = [0.32, 0.877, 0.877, 0.877, 0.877, 0.877, 0.877]  #mm^2
 
-I = [xxx, xxx, xxx]  ######Tank
+I = [1.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]  #mm^4
 
 #Define material properties.
-E = [29000.0]
+E = [30.0] #kN/mm^2 
 
 #Define nodal coordinates.
-node_coordinates = [[0.0, 0.0], [24.0, -48.0], [48.0, 0.0]]
+node_coordinates = [[0.0, -12000.0], [6000.0, -9000.0], [12000.0, -6000.0], [18000.0, -4000.0], [24000.0, -2500.0], [30000.0, -1000.0], [36000.0, -750.0], [40000.0, 0.0]]
 
 #Define element connectivity.
-element_connectivity = [(1, 2), (2,3), (1,3)]
+element_connectivity = [(1, 2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,8)]
 
 #Define element material properties
-element_mat_props = [1, 1, 1]
+element_mat_props = [1, 1, 1, 1, 1, 1, 1]
 
 #Define element section properties
-element_sect_props = [1, 1, 2]
+element_sect_props = [1, 2, 3, 4, 5, 6, 7]
 
 #Define support degrees of freedom.
-supports = [1, 2, 3, 5]
+supports = [1, 2, 22]
 
 #Define external loads.
-num_dof = size(node_coordinates, 1) * 3  #3 dof per node   #update this Tank
+num_dof = size(node_coordinates, 1) * 3  #3 dof per node   
 F = zeros(Float64, num_dof)
-F[6] = -1.875 #kips
+F[2] = -1.875 #kN
+F[5] = -1.875 
+F[8] = -1.875 
+F[11] = -1.875 
+F[14] = -1.875 
+F[17] = -1.875 
+F[20] = -1.875 
+F[23] = -1.875 
 
 #Define imposed support displacements.
-us = [0.0, 0.0, 0.0, 0.0]
+us = [0.0, 0.0, 0.0]
 
 
 #We could also define imposed temperature changes here if we wanted to, at some point...
@@ -67,33 +74,55 @@ end
 θ = [calculate_element_orientation(node_coordinates[element_connectivity[i][1]], node_coordinates[element_connectivity[i][2]]) for i in eachindex(element_connectivity)]
 
 
-### this will change, add frame element, matrix is symmetric Tank 
+
 #local element stiffness matrix
-function define_k_local(E, A, L)
+function define_k_local(E, I, A, L)
     
-    k_local = zeros(Float64, 4, 4)
+    k_local = zeros(Float64, 6, 6)
+
     k_local[1, 1] = E*A/L
-    k_local[1, 3] = -E*A/L
-    k_local[3, 1] = -E*A/L
-    k_local[3, 3] = E*A/L
+    k_local[1, 4] = -E*A/L
+    k_local[2, 2] = 12*E*I/L^3
+    k_local[2, 3] = 6*E*I/L^2
+    k_local[2, 5] = -12*E*I/L^3
+    k_local[2, 6] = 6*E*I/L^2
+    k_local[3, 3] = 4*E*I/L
+    k_local[3, 5] =  -6*E*I/L^2
+    k_local[3, 6] = 2*E*I/L
+    k_local[4, 4] = E*A/L
+    k_local[5, 5] = 12*E*I/L^3
+    k_local[5, 6] = -6*E*I/L^2
+    k_local[6, 6] = 4*E*I/L
+
+    #reflect symmetric values that aren't on the diagonal
+    i = [1, 2, 2, 2, 3, 3, 5]
+    j = [4, 3, 5, 6, 5, 6, 6]
+    
+    for z in eachindex(i)
+        k_local[j[z], i[z]] = k_local[i[z], j[z]]
+    end
+
 
     return k_local
 
 end
 
-k_local = [define_k_local(E[element_mat_props[i]], A[element_sect_props[i]], L[i]) for i in eachindex(element_connectivity)]
+k_local = [define_k_local(E[element_mat_props[i]], I[element_sect_props[i]], A[element_sect_props[i]], L[i]) for i in eachindex(element_connectivity)]
 
-#update this... Tank 
+
 #rotation matrix 
 function define_rotation_matrix(θ)
 
-    β = zeros(Float64, 4, 4)
+    β = zeros(Float64, 6, 6)
 
     unit_rotation = [cos(θ) sin(θ)
                      -sin(θ) cos(θ)]
 
+    β[3, 3] = 1.0
+    β[6, 6] = 1.0
+
     β[1:2, 1:2] = unit_rotation
-    β[3:4, 3:4] = unit_rotation
+    β[4:5, 4:5] = unit_rotation
 
     return β
 
@@ -116,11 +145,11 @@ k_global = [transform_local_to_global_element_stiffness(β[i], k_local[i]) for i
 #define global dof associated with each element 
 function define_global_dof_for_element(node_number_i, node_number_j)
 
-    #change these 
-    dof_i = [node_number_i * 2 - 1, node_number_i * 2, node_number_i * 2 + 1]
-    dof_j = [node_number_j * 2 - 1, node_number_j * 2, node_number_j * 2 + 1]
+    
+    dof_i = [node_number_i * 3 - 2, node_number_i * 3 - 1, node_number_i * 3]
+    dof_j = [node_number_j * 3 - 2, node_number_j * 3 - 1, node_number_j * 3]
 
-    dof = [dof_i; dof_j]   # [1, 2],  [3, 4]  => [1, 2, 3, 4]   
+    dof = [dof_i; dof_j]   # [1, 2, 3],  [4, 5, 6]  => [1, 2, 3, 4, 5, 6]   
 
     return dof
 
